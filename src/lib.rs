5a6462f192b0d2664d39165b86d3405a0b52ad99
aumetra <aumetra@cryptolab.net>
2023-06-06 15:54:18 +0200

    Make compatible with Shuttle CLI

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ use diesel_migrations_async::{EmbeddedMigrations, MigrationHarness};
 use serde::Serialize;
 use shuttle_service::{
     database::{SharedEngine, Type as DatabaseType},
-    Factory, ResourceBuilder, Type,
+    DbInput, DbOutput, Factory, ResourceBuilder, Type,
 };
 
 pub use diesel_async;
@@ -15,24 +15,34 @@ pub use diesel_migrations_async;
 
 #[derive(Default, Serialize)]
 pub struct Postgres {
+    #[serde(flatten)]
+    db_input: DbInput,
     #[serde(skip)]
     migrations: Option<EmbeddedMigrations>,
-    pool_size: Option<usize>,
 }
 
 impl Postgres {
+    pub fn local_uri(self, local_uri: impl ToString) -> Self {
+        Self {
+            db_input: DbInput {
+                local_uri: Some(local_uri.to_string()),
+            },
+            ..self
+        }
+    }
+
     pub fn migrations(self, migrations: EmbeddedMigrations) -> Self {
         Self {
             migrations: Some(migrations),
             ..self
         }
     }
+}
 
-    pub fn pool_size(self, pool_size: usize) -> Self {
-        Self {
-            pool_size: Some(pool_size),
-            ..self
-        }
+fn get_connection_string(db_output: &DbOutput) -> String {
+    match db_output {
+        DbOutput::Info(ref info) => info.connection_string_private(),
+        DbOutput::Local(ref local) => local.clone(),
     }
 }
 
@@ -41,7 +51,7 @@ impl ResourceBuilder<Pool<AsyncPgConnection>> for Postgres {
     const TYPE: Type = Type::Database(DatabaseType::Shared(SharedEngine::Postgres));
 
     type Config = Self;
-    type Output = (String, Option<usize>);
+    type Output = DbOutput;
 
     fn new() -> Self {
         Self::default()
@@ -55,12 +65,17 @@ impl ResourceBuilder<Pool<AsyncPgConnection>> for Postgres {
         self,
         factory: &mut dyn Factory,
     ) -> Result<Self::Output, shuttle_service::Error> {
-        let conn_data = factory
-            .get_db_connection(DatabaseType::Shared(SharedEngine::Postgres))
-            .await?;
-        let conn_string = conn_data.connection_string_private();
+        let db_output = if let Some(local_uri) = self.db_input.local_uri {
+            DbOutput::Local(local_uri)
+        } else {
+            let conn_data = factory
+                .get_db_connection(DatabaseType::Shared(SharedEngine::Postgres))
+                .await?;
+            DbOutput::Info(conn_data)
+        };
 
         if let Some(migrations) = self.migrations {
+            let conn_string = get_connection_string(&db_output);
             let mut conn = AsyncPgConnection::establish(&conn_string)
                 .await
                 .map_err(|err| shuttle_service::Error::Custom(err.into()))?;
@@ -68,18 +83,16 @@ impl ResourceBuilder<Pool<AsyncPgConnection>> for Postgres {
             conn.run_pending_migrations(migrations).await.unwrap();
         }
 
-        Ok((conn_string, self.pool_size))
+        Ok(db_output)
     }
 
     async fn build(
-        (conn_string, pool_size): &Self::Output,
+        db_output: &Self::Output,
     ) -> Result<Pool<AsyncPgConnection>, shuttle_service::Error> {
+        let conn_string = get_connection_string(db_output);
         let config = AsyncDieselConnectionManager::new(conn_string);
-        let mut pool = Pool::builder(config);
-        if let Some(pool_size) = pool_size {
-            pool = pool.max_size(*pool_size);
-        }
-
-        pool.build().map_err(|err| shuttle_service::Error::Custom(err.into()))
+        Pool::builder(config)
+            .build()
+            .map_err(|err| shuttle_service::Error::Custom(err.into()))
     }
 }
